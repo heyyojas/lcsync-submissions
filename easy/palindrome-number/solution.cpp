bc352abaa62280969f8306dbd2ed7f1c1d23feab
Ojas Sharma <sharmaojas56@gmail.com>
2026-07-23 18:42:24 +0530

    Add solution: Palindrome Number (Easy)

---
+++ easy/palindrome-number/solution.cpp
@@ -0,0 +1,18 @@
+// Palindrome Number
+// Difficulty: Easy
+// https://leetcode.com/problems/palindrome-number/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        
+        int original = x;
+        int long reversed = 0;
+        while(x!=0){
+            reversed = reversed*10 + (x % 10);
+            x=x/10;
+        }
+        return original == reversed;
+    }
+};
