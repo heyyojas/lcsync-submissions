c601245d12c277d9a88d4c172b7846ac1123c96e
Ojas Sharma <sharmaojas56@gmail.com>
2026-07-23 18:41:50 +0530

    Add solution: Smallest Stable Index I (Easy)

---
+++ easy/smallest-stable-index-i/solution.cpp
@@ -0,0 +1,28 @@
+// Smallest Stable Index I
+// Difficulty: Easy
+// https://leetcode.com/problems/smallest-stable-index-i/
+
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> max1(n), min1(n);
+
+        max1[0] = nums[0];
+        for(int i=1; i<n; i++){
+            max1[i] = max(max1[i-1], nums[i]);
+        }
+
+        min1[n-1] = nums[n-1];
+        for(int i=n-2; i>=0; i--){
+            min1[i] = min(min1[i+1], nums[i]);
+        }
+
+        for(int i=0; i<n; i++){
+            if(max1[i] - min1[i] <=k){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
