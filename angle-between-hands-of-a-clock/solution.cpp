bd33789b018621a0798e0ed22b4b2a16e418041f
Ojas Sharma <sharmaojas56@gmail.com>
2026-07-23 18:25:23 +0530

    Add solution: Angle Between Hands of a Clock

---
+++ angle-between-hands-of-a-clock/solution.cpp
@@ -0,0 +1,14 @@
+// Angle Between Hands of a Clock
+// https://leetcode.com/problems/angle-between-hands-of-a-clock/
+
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        double min_angle = minutes*6;
+        double hour_angle = (hour%12)*30 + minutes *0.5;
+
+        double diff = abs(hour_angle - min_angle);
+        
+        return min(diff, 360 - diff);
+    }
+};
