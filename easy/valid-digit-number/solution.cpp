7ae1fb121db099043018ffefed63c9d19fd8d705
Ojas Sharma <sharmaojas56@gmail.com>
2026-07-23 18:40:58 +0530

    Add solution: Valid Digit Number (Easy)

---
+++ easy/valid-digit-number/solution.cpp
@@ -0,0 +1,18 @@
+// Valid Digit Number
+// Difficulty: Easy
+// https://leetcode.com/problems/valid-digit-number/
+
+class Solution {
+public:
+    bool validDigit(int n, int x) {
+        int first = n;
+        while(first>=10) first/=10;
+        if(first==x) return false;
+
+        while(n>0){
+            if (n%10 == x) return true;
+            n/=10;
+        }
+        return false;
+    }
+};
