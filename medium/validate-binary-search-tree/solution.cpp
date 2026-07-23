dc3c86ef8bbe9853e65ae6497759b11e93f341af
Ojas Sharma <sharmaojas56@gmail.com>
2026-07-23 18:41:48 +0530

    Add solution: Validate Binary Search Tree (Medium)

---
+++ medium/validate-binary-search-tree/solution.cpp
@@ -0,0 +1,33 @@
+// Validate Binary Search Tree
+// Difficulty: Medium
+// https://leetcode.com/problems/validate-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    TreeNode* prev = NULL;
+
+    bool isValidBST(TreeNode* root) {
+        if(root==NULL) return true;
+
+        if(!isValidBST(root->left)) return false;
+        if(prev!=NULL && root->val <= prev->val){
+            return false;
+        }
+
+        prev = root;
+
+        return isValidBST(root->right);
+    }
+};
