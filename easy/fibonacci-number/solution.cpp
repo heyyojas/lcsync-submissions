297e3767b4aba7929f32cf72fb57986ecf04a827
Ojas Sharma <sharmaojas56@gmail.com>
2026-07-23 18:42:22 +0530

    Add solution: Fibonacci Number (Easy)

---
+++ easy/fibonacci-number/solution.cpp
@@ -0,0 +1,18 @@
+// Fibonacci Number
+// Difficulty: Easy
+// https://leetcode.com/problems/fibonacci-number/
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1) return n;
+        
+        int a=0, b=1;
+        for(int i=2; i<=n; i++){
+            int c= a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+};
