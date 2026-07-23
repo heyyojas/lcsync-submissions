2551b15d8dd5556ef6b048b31c90a0f436ebb5ed
Ojas Sharma <sharmaojas56@gmail.com>
2026-07-23 18:39:18 +0530

    Add solution: Unique Binary Search Trees (Medium)

---
+++ medium/unique-binary-search-trees/solution.cpp
@@ -0,0 +1,14 @@
+// Unique Binary Search Trees
+// Difficulty: Medium
+// https://leetcode.com/problems/unique-binary-search-trees/
+
+class Solution {
+public:
+    int numTrees(int n) {
+        long long C = 1;
+        for(int i=0; i<n; ++i){
+            C = C * 2 * (2 * i + 1) / (i + 2);
+        }
+        return C;
+    }
+};
