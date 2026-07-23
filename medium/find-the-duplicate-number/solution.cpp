41d65e0c41ec77ae43fc40c5ff556b2db8f703fa
Ojas Sharma <sharmaojas56@gmail.com>
2026-07-23 18:40:43 +0530

    Add solution: Find the Duplicate Number (Medium)

---
+++ medium/find-the-duplicate-number/solution.cpp
@@ -0,0 +1,25 @@
+// Find the Duplicate Number
+// Difficulty: Medium
+// https://leetcode.com/problems/find-the-duplicate-number/
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int fast = nums[0];
+        int slow = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+
+
+        slow = nums[0];
+        while(fast!=slow){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+};
