0b28de595ea9fc12673688916a1eb16d8bb73261
Ojas Sharma <sharmaojas56@gmail.com>
2026-07-23 18:29:05 +0530

    Add solution: Top K Frequent Elements (Medium)

---
+++ medium/top-k-frequent-elements/solution.cpp
@@ -0,0 +1,33 @@
+// Top K Frequent Elements
+// Difficulty: Medium
+// https://leetcode.com/problems/top-k-frequent-elements/
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+
+        for(int num : nums){
+            freq[num]++;
+        }
+
+        vector<vector<int>> bucket(nums.size() + 1);
+
+        for(auto &it : freq){
+            bucket[it.second].push_back(it.first);
+        }
+
+        vector<int> ans; 
+
+        for(int i = nums.size(); i>=1 && ans.size() < k; i--){
+            for(int num : bucket[i]){
+                ans.push_back(num);
+                if(ans.size() == k){
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
