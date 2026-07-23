feade154926d5f4804cafa2587d6a49464b3f8e4
Ojas Sharma <sharmaojas56@gmail.com>
2026-07-23 18:39:59 +0530

    Add solution: Valid Palindrome (Easy)

---
+++ easy/valid-palindrome/solution.cpp
@@ -0,0 +1,22 @@
+// Valid Palindrome
+// Difficulty: Easy
+// https://leetcode.com/problems/valid-palindrome/
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.size()-1;
+        while(left<right){
+            while(left<right && !isalnum(s[left])) left++;
+            while (left < right && !isalnum(s[right])) right--;
+            
+            if(tolower(s[left]) != tolower(s[right])){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
