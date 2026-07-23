d33a4a5985f355cc0ac2b8722abe1f7f09eaea2b
Ojas Sharma <sharmaojas56@gmail.com>
2026-07-23 18:40:22 +0530

    Add solution: Count Indices With Opposite Parity (Easy)

---
+++ easy/count-indices-with-opposite-parity/solution.cpp
@@ -0,0 +1,23 @@
+// Count Indices With Opposite Parity
+// Difficulty: Easy
+// https://leetcode.com/problems/count-indices-with-opposite-parity/
+
+class Solution {
+public:
+    vector<int> countOppositeParity(vector<int>& nums) {
+        vector<int> ans(nums.size());
+        int even = 0;
+        int odd = 0;
+        
+        for(int i=nums.size()-1; i>=0; i--){
+            if(nums[i] % 2 == 0){
+                ans[i] = odd;
+                even++;
+            }else{
+                ans[i] = even;
+                odd++;
+            }
+        }
+        return ans;
+    }
+};
