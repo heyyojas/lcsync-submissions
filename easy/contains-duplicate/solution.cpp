2649dda4b2105676b2b5ea84f65e18b2c780c469
Ojas Sharma <sharmaojas56@gmail.com>
2026-07-31 20:08:43 +0530

    Add solution: Contains Duplicate (Easy)

---
+++ easy/contains-duplicate/solution.cpp
@@ -0,0 +1,19 @@
+// Contains Duplicate
+// Difficulty: Easy
+// https://leetcode.com/problems/contains-duplicate/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+
+        for(int x : nums){
+            if(seen.count(x))
+                return true;
+
+            seen.insert(x);
+        }
+
+        return false;
+    }
+};
