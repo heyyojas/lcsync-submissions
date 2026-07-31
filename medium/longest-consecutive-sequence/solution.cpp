f3d678c9cf47ebb311662298b3f26a80ea5d4154
Ojas Sharma <sharmaojas56@gmail.com>
2026-07-31 20:08:33 +0530

    Add solution: Longest Consecutive Sequence (Medium)

---
+++ medium/longest-consecutive-sequence/solution.cpp
@@ -0,0 +1,27 @@
+// Longest Consecutive Sequence
+// Difficulty: Medium
+// https://leetcode.com/problems/longest-consecutive-sequence/
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> st(nums.begin(), nums.end());
+
+        int longest = 0;
+
+        for(int num : st){
+            if(!st.count(num-1)){
+                int current = num;
+                int length = 1;
+
+                while(st.count(current + 1)){
+                    current++;
+                    length++;
+                }
+
+                longest = max(longest, length);
+            }
+        }
+        return longest;
+    }
+};
