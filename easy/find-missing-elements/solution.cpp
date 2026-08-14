bae68efdc04e396aff99a8b4f0db589294afec9f
Ojas Sharma <sharmaojas56@gmail.com>
2026-08-14 15:01:46 +0530

    Add solution: Find Missing Elements (Easy)

---
+++ easy/find-missing-elements/solution.cpp
@@ -0,0 +1,19 @@
+// Find Missing Elements
+// Difficulty: Easy
+// https://leetcode.com/problems/find-missing-elements/
+
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        vector<int> ans;
+
+        for(int i=0; i < nums.size() -1 ; i++ ){
+            for(int x = nums[i] + 1 ; x < nums[i + 1]; x++ ){
+                ans.push_back(x);
+            }
+        }
+        return ans;
+    }
+};
