853e5accae65fe7d93cd35064c360f372b1b7fb8
Ojas Sharma <sharmaojas56@gmail.com>
2026-08-14 15:01:40 +0530

    Add solution: Find the Highest Altitude (Easy)

---
+++ easy/find-the-highest-altitude/solution.cpp
@@ -0,0 +1,17 @@
+// Find the Highest Altitude
+// Difficulty: Easy
+// https://leetcode.com/problems/find-the-highest-altitude/
+
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int altitude = 0;
+        int highest = 0;
+
+        for(int i=0; i<gain.size(); i++){
+            altitude += gain[i];
+            highest = max(highest, altitude);
+        }
+        return highest;
+    }
+};
